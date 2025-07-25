81c57abfb4f9b31f0d3e6887a96993a91d44d06a
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-07-25 12:25:24 +0530

    GetById commit

--- src/main/java/com/practice/GetDataById.java
+++ src/main/java/com/practice/GetDataById.java
@@ -20,9 +20,26 @@ public class GetDataById {
 		//student st = session.get(student.class,2);
 		
 		//new method
+		//Session level cache
 		Student st = session.find(Student.class, 2);
 		System.out.println(st.toString());
+		Student st1 = session.find(Student.class, 2);
+		System.out.println(st1.toString());
+		Student st2 = session.find(Student.class, 2);
+		System.out.println(st2.toString());
+
 		transaction.commit();
 		session.close();
+		
+		//new session creation 
+//		Session session2 = factory.openSession();
+//		Transaction transaction2 = session2.beginTransaction();
+//		
+//		Student st3 = session.find(Student.class, 2);
+//		System.out.println(st3.toString());
+//		
+//		transaction2.commit();
+//		session2.close();
+		
 	}
 }
