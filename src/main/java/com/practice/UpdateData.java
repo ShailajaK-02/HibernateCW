0ce1d6927e3e70b538a167679e984daca5b5e76f
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-08-20 18:44:54 +0530

    update data modified

--- src/main/java/com/practice/UpdateData.java
+++ src/main/java/com/practice/UpdateData.java
@@ -7,6 +7,7 @@ import org.hibernate.Transaction;
 import com.practice.Entity.Student;
 import com.practice.Utility.HibernateUtility;
 
+//update data
 public class UpdateData {
 	
 	public static void main(String[] args) {
